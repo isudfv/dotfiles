b4392b16c2b289add1465af72a9f23240dcfdbeb
韩希贤 <hanxixian.isudfv@bytedance.com>
2025-11-05 12:04:20 +0800

    update colorscheme

--- dot_config/nvim/lua/plugins_nvim/colorscheme.lua
+++ dot_config/nvim/lua/plugins_nvim/colorscheme.lua
@@ -14,9 +14,36 @@ return {
 	},
 	{
 		"miikanissi/modus-themes.nvim",
+		enabled = false,
 		priority = 1000,
 		config = function ()
 			vim.cmd.colorscheme('modus')
 		end
+	},
+	{
+		"m00qek/baleia.nvim",
+		version = "*",
+		enabled = true,
+		config = function()
+			vim.g.baleia = require("baleia").setup({ log = 'DEBUG' })
+
+			-- Command to colorize the current buffer
+			vim.api.nvim_create_user_command("BaleiaColorize", function()
+				vim.g.baleia.once(vim.api.nvim_get_current_buf())
+			end, { bang = true })
+
+			-- Command to show logs
+			vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+			-- auto coloize  quickfix
+			vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+				pattern = "quickfix",
+				callback = function()
+					vim.api.nvim_set_option_value("modifiable", true, { buf = buffer })
+					vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
+					vim.api.nvim_set_option_value("modified", false, { buf = buffer })
+					vim.api.nvim_set_option_value("modifiable", false, { buf = buffer })
+				end,
+			})
+		end,
 	}
 }
