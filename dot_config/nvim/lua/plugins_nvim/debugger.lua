e026ff6f8b53fcd2f9d52376f1a51a2347feb946
isudfv <isudfv@outlook.com>
2025-11-15 11:03:29 +0800

    update dap keymap

--- dot_config/nvim/lua/plugins_nvim/debugger.lua
+++ dot_config/nvim/lua/plugins_nvim/debugger.lua
@@ -62,13 +62,38 @@ return {
 				dapui.close()
 			end
 
-			-- keymaps
-			vim.keymap.set('n', '<F8>', function() dap.step_over() end, { desc = "DAP Step Over" })
-			vim.keymap.set('n', '<F7>', function() dap.step_into() end, { desc = "DAP Step Into" })
-			vim.keymap.set('n', '<S-F7>', function() dap.step_out() end, { desc = "DAP Step Out" })
-			vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
-			vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = "DAP Continue" })
-			vim.keymap.set({'n', 'v'}, '<leader>qe', function() dapui.eval() end, { desc = "DAP Quick Evaluate" })
-		end
-	}
+            vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+            vim.keymap.set('n', '<F8>', function() require('dap').step_over() end)
+            vim.keymap.set('n', '<F7>', function() require('dap').step_into() end)
+            vim.keymap.set('n', '<S-F7>', function() require('dap').step_out() end)
+            vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+            vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+            vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+            vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+            vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+            vim.keymap.set({'n', 'v'}, '<leader>qe', function() dapui.eval() end, { desc = "DAP Quick Evaluate" })
+            vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+                require('dap.ui.widgets').hover()
+            end)
+            vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+                require('dap.ui.widgets').preview()
+            end)
+            vim.keymap.set('n', '<Leader>df', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.frames)
+            end)
+            vim.keymap.set('n', '<Leader>ds', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.scopes)
+            end)
+        end
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        opts = {
+        },
+        config = function()
+            require("dap-python").setup("uv")
+        end
+    }
 }
